8f004355c726a63ec7637f6b55ac6841712da21e
Fabien Pereira <fabpereira75@gmail.com>
2021-03-16 22:27:31 +0100

    retouche css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-const formValidation = document.getElementById("form-validation");
+const formInscription = document.getElementById("inscription");
+const champAge = formInscription.elements["age"];
+const champPrenom = formInscription.elements["prenom"];
+const errorMsg = document.getElementById("error");
+
+function valider (e) {
+    if(isNaN(champAge.value)) {
+        errorMsg.classList.add("showDivError");
+    } else {
+        
+    }
+}
 
 
 
-formValidation.addEventListener('submit', valider);
+formInscription.addEventListener('submit', valider);
